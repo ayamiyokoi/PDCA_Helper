060f061ee26178ae057f5613438e2f1b248dfd35
AyamiYokoi <didufd63hff@gmail.com>
2021-05-21 06:27:30 +0000

    [delete]topicspage

--- app/controllers/reflections_controller.rb
+++ app/controllers/reflections_controller.rb
@@ -56,10 +56,6 @@ class ReflectionsController < ApplicationController
     end
   end
 
-  def topics
-    @reflection = Reflection.all
-  end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_reflection
