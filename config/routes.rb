2a51ee8edf6c764a90af34384219fed0825a3c3a
AyamiYokoi <didufd63hff@gmail.com>
2021-05-21 02:11:16 +0000

    [Add]user index

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :reflections
   root :to =>'reflections#top'
+  resources :users, :only => [:index, :show]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
