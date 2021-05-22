c110e49d73abbb8cc72337fb5b11fbab67208419
AyamiYokoi <didufd63hff@gmail.com>
2021-05-22 08:15:25 +0000

    [Add]update method(user)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+
   def edit
-   
+
   end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :reflections
   root :to =>'reflections#top'
-  resources :users, :only => [:index, :show, :edit]
+  resources :users
   get 'reflections/topics' => 'reflections#topics', as: 'topics'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
