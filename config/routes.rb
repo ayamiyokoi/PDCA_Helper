5ec4927189682d0439e7fe747c0098886ecbf87b
AyamiYokoi <didufd63hff@gmail.com>
2021-05-26 05:24:44 +0000

    どうしたらいいかわからない

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,16 @@
 Rails.application.routes.draw do
   get 'topics/index'
   devise_for :users
-  resources :reflections
+  resources :reflections do
+    resource :favorites, only: [:create, :destroy]
+  end
   root :to =>'reflections#top'
-  resources :users
+  resources :users do
+    get :favorites, on: :collection
+  end
   resources :groups, only: [:new, :create, :edit, :update, :index]
   resources :topics, only: [:index]
   resources :favorites, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit, :update] do
-    get :favorites, on: :collection
-  end
 
-  resources :reflections, expect: [:index] do
-    resource :favorites, only: [:create, :destroy]
-  end
 end
